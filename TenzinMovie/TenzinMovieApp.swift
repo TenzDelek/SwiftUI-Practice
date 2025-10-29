ece3d23984f79c68f01d3fca0710a8e73eb58ebb
TenzDelek <tibetdelek@gmail.com>
2025-10-29 09:58:56 +0530

    Initial Commit

---
+++ TenzinMovie/TenzinMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenzinMovieApp.swift
+//  TenzinMovie
+//
+//  Created by Tenzin Delek on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TenzinMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
